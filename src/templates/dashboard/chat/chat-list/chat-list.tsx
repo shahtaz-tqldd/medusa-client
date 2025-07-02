25d663b60f046c984525cdf7ddf8cd20adbe808f
Shahtaz Rahman <shahtazrahman17@gmail.com>
2025-07-03 03:52:14 +0600

    messages ui

--- src/templates/dashboard/chat/chat-list/chat-list.tsx
+++ src/templates/dashboard/chat/chat-list/chat-list.tsx
@@ -1,24 +1,64 @@
+import PrimaryCard from "@/components/cards/primary-card";
+import Pagination from "@/components/reusable/pagination";
+import BodyText from "@/components/text/body-text";
+import { Conversation } from "@/lib/chat-service";
+import { formatTimeFromNow } from "@/lib/date";
 import React from "react";
 
-interface ChatListProps{
-    className: string;
+interface ChatListProps {
+  className?: string;
+  conversations: Conversation[];
+  isLoading: boolean;
+  page: number;
+  total: number;
+  setPage: (page: number) => void;
+  onSelect: (conversation: Conversation) => void;
 }
 
-const ChatList = ({className}: ChatListProps) => {
+const ChatList = ({
+  className,
+  conversations,
+  isLoading,
+  page,
+  total,
+  setPage,
+  onSelect,
+}: ChatListProps) => {
   return (
     <div className={`flex flex-col ${className}`}>
-        <div className="bg-white/5 p-3">
-            <span>Chat List</span>
-        </div>
-        <div className="flex-1 overflow-y-auto p-2 space-y-2">
-            <div className="p-3 rounded-lg bg-white/5 cursor-pointer hover:dark:bg-white/10 tr">
-                <h2 className="text-sm font-medium">Hi, I want to talk about...</h2>
-                <div className="flbx text-xs opacity-60">
-                    <p className="">Dhaka, Bangladesh</p>
-                    <p className="">1hr ago</p>
-                </div>
-            </div>
-        </div>
+      <div className="dark:bg-white/5 bg-blue-500/10 p-3">
+        <h2 className="font-medium text-base">Chat List</h2>
+      </div>
+
+      <div className="flex-1 medusa-scroll p-2 space-y-2">
+        {isLoading ? (
+          <p className="text-sm text-white/50">Loading conversations...</p>
+        ) : conversations.length === 0 ? (
+          <p className="text-sm text-white/50">No conversations found.</p>
+        ) : (
+          conversations?.map((conversation) => (
+            <PrimaryCard
+              key={conversation.id}
+              className="p-3 md:p-3 lg:p-3 rounded-xl cursor-pointer hover:bg-white/10 transition"
+              onClick={() => onSelect(conversation)}
+            >
+              <h2 className="text-sm font-medium truncate">
+                {conversation.title || "Untitled conversation"}
+              </h2>
+
+              {conversation.last_message?.content && (
+                <BodyText className="mt-1 text-[13px] line-clamp-2">
+                  {conversation.last_message.content}
+                </BodyText>
+              )}
+              <div className="mt-4 text-xs opacity-60 gap-2">
+                <p>{formatTimeFromNow(conversation.created_at)}</p>
+              </div>
+            </PrimaryCard>
+          ))
+        )}
+      </div>
+      <Pagination page={page} setPage={setPage} totalCount={total} />
     </div>
   );
 };
--- src/templates/dashboard/chat/index.tsx
+++ src/templates/dashboard/chat/index.tsx
@@ -1,16 +1,49 @@
-import React from "react";
+"use client";
+
+import React, { useEffect, useState } from "react";
 import PageTitle from "@/components/reusable/page-title";
 import ChatList from "./chat-list/chat-list";
 import ChatDetails from "./chat-details/chat-details";
+import { Conversation, fetchConversationList } from "@/lib/chat-service";
 
 const ChatPage = () => {
+  const [conversations, setConversations] = useState<Conversation[]>([]);
+  const [page, setPage] = useState<number>(1);
+  const [total, setTotal] = useState<number>(0);
+  const [search, setSearch] = useState<string>("");
+  const [selectedConversation, setSelectedConversation] =
+    useState<Conversation | null>(null);
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+
+  useEffect(() => {
+    const loadConversations = async () => {
+      setIsLoading(true);
+      const { convos, total } = await fetchConversationList(page, search);
+      setConversations(convos);
+      setTotal(total);
+      setIsLoading(false);
+    };
+
+    loadConversations();
+  }, []);
+
   return (
     <div className="h-full flex flex-col">
-        <PageTitle>Chat</PageTitle>
-        <div className="mt-8 flex flex-1 rounded-2xl overflow-hidden border-2 border-white/10">
-            <ChatList className="w-[360px] border-r border-r-white/10"/>
-            <ChatDetails className=""/>
-        </div>
+      <PageTitle>Chat</PageTitle>
+      <div className="mt-8 flex flex-1 rounded-2xl overflow-hidden border-2 dark:border-white/10 border-blue-500/10">
+        <ChatList
+          className="w-[400px] border-r dark:border-r-white/10 border-r-blue-500/15"
+          conversations={conversations}
+          isLoading={isLoading}
+          onSelect={(conversation) => {
+            setSelectedConversation(conversation);
+          }}
+          page={page}
+          setPage={setPage}
+          total={total}
+        />
+        <ChatDetails className="" conversation={selectedConversation} />
+      </div>
     </div>
   );
 };
