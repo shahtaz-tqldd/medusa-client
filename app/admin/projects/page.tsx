023cb605cac8439cf7fba431101084f0fbc3a9c4
shahtaz <shahtazrahman17@gmail.com>
2025-12-30 02:22:12 +0600

    project page added

---
+++ app/admin/projects/page.tsx
@@ -0,0 +1,10 @@
+import DashboardLayout from "@/layouts/admin-layout";
+import ProjectPage from "@/templates/admin/projects";
+
+export default function AdminProjectPage() {
+  return (
+    <DashboardLayout>
+      <ProjectPage />
+    </DashboardLayout>
+  );
+}
